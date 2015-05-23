f94ff76f2d98dc028439e38853abaed9d082e151
Tomas Krizek <t@krizek.ws>
2015-05-23 13:34:20 +0200

    app: database background thread support

--- src/app/database.py
+++ src/app/database.py
@@ -1,42 +1,49 @@
 """
 RDB 2015
 
-Database Layer
+UI-Database connection
 
-Connection driver
+Database Manager
 
 Author: Tomas Krizek
-
 """
 
+from PyQt5.QtCore import pyqtSlot, pyqtSignal, QThread, QMutex, QMutexLocker
 import psycopg2
 from datetime import datetime
 
 
 REMOTE = {
     'host': '147.230.21.34',
-    'database': 'RDB2015_DanielMadera',
+    'database': 'rdb2015_danielmadera',
     'user': 'student',
     'password': 'student'
 }
 
 LOCAL = {
     'host': '127.0.0.1',
-    'database': 'RDB2015_DanielMadera',
+    'database': 'rdb2015_danielmadera',
     'user': 'postgres',
     'password': 'student'
 }
 
 
-def connect(location=REMOTE):
+def connect(location=LOCAL):
     """Opens and returns a database connection."""
     conn = psycopg2.connect(**location)
     with conn.cursor() as cur:
         cur.execute("set schema 'rdb'")
+    conn.commit()
     return conn
 
 
-def get_devices(conn):
+conMutex = QMutex()
+conn = connect(LOCAL)
+conMutex.unlock()
+
+
+def get_devices():
+    QMutexLocker(conMutex)
     """Retrieves all devices from database."""
     sql = 'select "serial_number", "description" from "devices"'
     with conn.cursor() as cur:
@@ -44,32 +51,36 @@ def get_devices(conn):
         return cur.fetchall()
 
 
-def remove_device(conn, serial_number):
+def remove_device(serial_number):
+    QMutexLocker(conMutex)
     sql = 'delete from "devices" where "serial_number" = (%s)'
     with conn.cursor() as cur:
         cur.execute(sql, (serial_number,))
     conn.commit()
 
 
-def get_blocks(conn):
+def get_blocks():
+    QMutexLocker(conMutex)
     sql = 'select "id", "description" from "blocks"'
     with conn.cursor() as cur:
         cur.execute(sql)
         return cur.fetchall()
 
 
-def remove_block(conn, id_):
+def remove_block(id_):
+    QMutexLocker(conMutex)
     sql = 'delete from "blocks" where "id" = (%s)'
     with conn.cursor() as cur:
         cur.execute(sql, (id_,))
     conn.commit()
 
 
-def import_data(conn, data):
+def import_data(data):
     """Performs multiple insert of data."""
-    sql = 'insert into rdb."raw_data"("timestamp", "unit", "location_id", \
+    QMutexLocker(conMutex)
+    sql = 'insert into rdb."raw_data_view"("created", "unit", "location_id", \
             "longitude", "latitude", "location_description", "value1", \
-            "value2", "unit_deviation", "device_sn", "device_description", \
+            "value2", "unit_deviation", "serial_number", "device_description", \
             "block_id", "block_description") values '
 
     with conn.cursor() as cur:
@@ -78,20 +89,20 @@ def import_data(conn, data):
     conn.commit()
 
 def _get_import_values_string(cur, row):
-    row[0] = datetime.fromtimestamp(int(row[0]))
+    row[0] = datetime.utcfromtimestamp(int(row[0]))
     return cur.mogrify("(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
         row).decode('utf-8')
 
-def export_data(conn, filename):
+def export_data(filename):
     """Exports all raw_data."""
-    # TODO export from view, not raw_data
+    QMutexLocker(conMutex)
     query = """
-    select (select round(extract(epoch from "timestamp" at time zone 'utc-2'))),\
+    select (select round(extract(epoch from "created" at time zone 'utc'))),\
             "unit", "location_id", \
             "longitude", "latitude", "location_description", "value1", \
-            "value2", "unit_deviation", "device_sn", "device_description", \
+            "value2", "unit_deviation", "serial_number", "device_description", \
             "block_id", "block_description"
-    from rdb."raw_data"
+    from rdb."raw_data_view"
     """
 
     outputquery = "COPY ({0}) TO STDOUT WITH CSV DELIMITER ';'".format(query)
@@ -99,3 +110,87 @@ def export_data(conn, filename):
     with conn.cursor() as cur:
         with open(filename, 'w') as f:
             cur.copy_expert(outputquery, f)
+
+
+
+
+def execute(function, callback=None, *args, **kwargs):
+    if callable(function):
+        queue.append(
+            {'function': function, \
+             'callback': callback, \
+             'args': args, \
+             'kwargs': kwargs})
+        _startExecution()
+
+
+def _startExecution():
+    if not thread.isRunning() and queue:
+        function = queue[0]['function']
+        args = queue[0]['args']
+        kwargs = queue[0]['kwargs']
+
+        thread.execute(function, *args, **kwargs)
+
+
+@pyqtSlot(dict)
+def _on_thread_executed(dict_):
+    callback = queue[0]['callback']
+    if callable(callback):
+        callback(dict_['returnValue'])
+    
+    del queue[0]
+
+    thread.wait()
+    _startExecution()
+
+
+class FunctionThread(QThread):
+    executed = pyqtSignal(dict)
+
+    def __init__(self, parent=None):
+        super(FunctionThread, self).__init__(parent)
+
+        self.mutex = QMutex()
+        
+        self.function = None
+        self.args = None
+        self.kwargs = None
+
+    def __del__(self):
+        self.wait()
+
+    def execute(self, function, *args, **kwargs):
+        locker = QMutexLocker(self.mutex)
+
+        self.function = function
+        self.args = args
+        self.kwargs = kwargs
+
+        self.wait()
+        self.start()
+
+    def run(self):
+        returnValue = self.function(*self.args, **self.kwargs)
+        self.executed.emit({'returnValue': returnValue})
+
+
+thread = FunctionThread()
+queue = []
+
+thread.executed.connect(_on_thread_executed)
+
+
+if __name__ == "__main__":
+    import sys
+    from PyQt5.QtCore import QCoreApplication
+    import signal
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+    app = QCoreApplication(sys.argv)
+    print('before')
+    execute(get_blocks, print)
+    print('between')
+    execute(get_devices, print)
+    print('after')
+    app.exec_()
