254471a7ba6c2015a8048af12abf4352c4ca6258
Daniel Madera <madera.dan@gmail.com>
2015-05-23 12:07:44 +0200

    merged create scripts, readme - removed instructions to create python procedure (not using)

--- db/create-db.sql
+++ db/create-db.sql
@@ -1,5 +1,8 @@
 -- psql -h 147.230.21.34 -U student -d postgres -f create-db.sql
 
+-- author: Daniel Madera
+-- description: creates database
+
 create database rdb2015_danielmadera
     with owner student
     encoding 'utf8' 
